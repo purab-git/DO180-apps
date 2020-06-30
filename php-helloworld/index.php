322327c97d04baee05d24220814a9c51b2a81a5d
purab-git <66621177+purab-git@users.noreply.github.com>
2020-06-30 19:48:25 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,8 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Hello, NEW Line  \n";
-echo "gethostname() \n"
+echo  gethostname()
+  echo "<br>";
+//get OS
+echo php_uname();
 ?>
